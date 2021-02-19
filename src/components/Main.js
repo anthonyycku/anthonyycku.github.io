8f145198fea30e90668815c4cfddbcf7c0bcfdb6
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-19 17:19:41 -0500

    text change

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ export default function Main(props) {
                 <br />
                 <p>I am a passionate software developer with a growth mindset aiming to provide high quality solutions to the world’s technological demands. I am a firm believer in that there is always space for growth and opportunities to learn.</p>
                 <p>I am responsible, a quick learner, flexible, and highly motivated by learning opportunities that bring success to both clients and my team.</p>
-                <p>Hope you have fun checking out my portfolio! If you'd like to connect with me for any reason, reach out to me - Let's talk.</p>
+                <p>Hope you have fun checking out my portfolio! If you'd like to connect with me for any reason, feel free to reach out to me - Let's talk.</p>
             </div>
         </div>
     )
